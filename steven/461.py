d8b580389c32302461bf0182f3445ee90456dd96
updownlife <xyliu.365@gmail.com>
2017-05-04 22:27:29 -0700

    add bitwise solution

--- steven/461.py
+++ steven/461.py
@@ -7,6 +7,19 @@ class Solution(object):
         """
         distance = 0
         xor = x ^ y
+        while xor:
+            distance += xor & 1
+            xor >>= 1
+        return distance
+    
+    def hammingDistance2(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        distance = 0
+        xor = x ^ y
         while xor > 0:
             distance += xor % 2
             xor /= 2
